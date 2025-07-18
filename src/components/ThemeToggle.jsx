6aff2e5cf03b0542e20c9d7e1e12c135702571eb
Ameeka-Patel <patel.ameek@northeastern.edu>
2025-07-18 14:49:19 -0700

    added a util function for class merging

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
-import { use, useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import {Sun, Moon} from "lucide-react";
+import {cn} from "@/lib/utils"; 
 
 export const ThemeToggle = () => {
 
@@ -34,7 +35,9 @@ export const ThemeToggle = () => {
 
 
     return (
-    <button onClick={toggleTheme}>
+    <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden"
+    )}>
         {isDarkMode ? (
             <Sun className="h-6 w-6 text-yellow-300" /> 
         ) : (
