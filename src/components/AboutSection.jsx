ef8505fb50b5a8b3e603f501051eab323ccd37ce
Ameeka-Patel <patel.ameek@northeastern.edu>
2025-07-19 18:19:59 -0700

    About Me section + resume button + skills on left side (languages, framework/libraries, tools)

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,3 +1,5 @@
+import { Code, Box, Wrench } from "lucide-react";
+
 export const AboutSection = () => {
     return (
         <section id="#about" className="py-24 px-4 relative">
@@ -6,28 +8,128 @@ export const AboutSection = () => {
                     about me
                 </h2>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-start">
                     {/* left column w/ text content */}
                     <div className="space-y-6">
-                        <h3 className="text-2xl font-semibold">header here</h3>
+                        {/* <h3 className="text-2xl font-semibold">header here</h3> */}
 
                         <p className="text-muted-foreground">
-                            ...
-                            ...
-                            ...
+                            i'm a rising 2nd year at Northeastern University with a combined
+                            major in Computer Science and Business Administration, concentrating
+                            in Marketing Analytics.
                         </p>
 
                         <p className="text-muted-foreground">
-                            ...
-                            ...
-                            ...
+                            i am always always eager to learn something new and further my
+                            web dev skills through Sandbox, FirstByte, or personal projects.
                         </p>
 
+                        <p className="text-muted-foreground">
+                            when i'm not working on these projects, you will find me
+                            baking/cooking (check out my recipes), going on long runs, or
+                            hanging out with friends
+                        </p>
+
+                        <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+                            <a href="#contact" className="cosmic-button">
+                                contact
+                            </a>
+                            <a href="BLANK.pdf" target="_blank" className="cosmic-button">
+                                resume
+                            </a>
+                        </div>
                     </div>
 
                     {/* right column w/ skills */}
-                    <div>
+                    <div className="grid grid-cols-1 gap-6">
 
+                        <div className="gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4">
+
+                                <div className="p-3 rounded-full bg-primary/10">
+                                    <Code className="h-6 w-6" />
+                                </div>
+
+                                <div className="text-left">
+                                    <h4 className="text-lg font-semibold">languages</h4>
+                                    <div className="flex space-x-3 text-center pt-4">
+                                        <div className="grid-cols-1 space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1"> Java </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> Python </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> TypeScript </p>
+                                        </div>
+
+                                        <div className="grid-cols-1  space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1"> JavaScript </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> HTML </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> CSS </p>
+                                        </div>
+
+                                        <div className="grid-cols-1  space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1"> Racket BSL </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> R </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4">
+                                <div className="p-3 rounded-full bg-primary/10">
+                                    <Box className="h-6 w-6" />
+                                </div>
+
+                                <div className="text-left">
+                                    <h4 className="text-lg font-semibold">libraries + frameworks</h4>
+                                    <div className="flex space-x-3 text-center pt-4">
+                                        <div className="grid-cols-1  space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1">React</p>
+                                            <p className="bg-primary/10 rounded-sm p-1">Next.js</p>
+                                            <p className="bg-primary/10 rounded-sm p-1">Node.js</p>
+                                        </div>
+                                        <div className="grid-cols-1  space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1">Prisma</p>
+                                            <p className="bg-primary/10 rounded-sm p-1">Tailwind CSS</p>
+                                            <p className="bg-primary/10 rounded-sm p-1">JUnit</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4">
+                                <div className="p-3 rounded-full bg-primary/10">
+                                    <Wrench className="h-6 w-6" />
+                                </div>
+
+                                <div className="text-left">
+                                    <h4 className="text-lg font-semibold">tools</h4>
+                                    <div className="flex space-x-3 text-center pt-4">
+                                        <div className="grid-cols-1  space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1"> Git </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> Docker </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> Vite </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> Vercel </p>
+                                        </div>
+                                        <div className="grid-cols-1  space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1"> VSCode </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> IntelliJ </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> Eclipse </p>
+                                        </div>
+                                        <div className="grid-cols-1  space-y-3">
+                                            <p className="bg-primary/10 rounded-sm p-1"> RStudio </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> Figma </p>
+                                            <p className="bg-primary/10 rounded-sm p-1"> Postman </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
 
                 </div>
