040e0907450c51cd5aff0194298865e8bfd9f5e6
Ameeka-Patel <patel.ameek@northeastern.edu>
2025-07-22 17:09:56 -0700

    changed bio in hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -15,7 +15,7 @@ export const HeroSection = () => {
                         </h1>
 
                         <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3 ">
-                            constantly curious + caffeinated
+                            student | developer | learner
                         </p>
 
                         <div className="pt-5 opacity-0 animate-fade-in-delay-4">
